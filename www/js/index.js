aa53e4eaa1c4103cad6643214656b44112756612
septidwic <septidwic96@gmail.com>
2018-05-28 18:01:55 +0700

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -3,7 +3,7 @@ var app = {
     // Application Constructor
     initialize: function() {
     
-        var url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
+        var url = "https://api.nasa.gov/planetary/apod?api_key=IJ4qy5TAUqLJCAEf7poIJgt86T29DODU7JE6GzVX";
         
         $("#loadImage").click(function(){
             $.ajax({
